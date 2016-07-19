61e3cc2eb745daad91c9d4b26e29bdda41e1039b
Warsheep-GER <xworshipxger@gmail.com>
2016-07-19 02:18:40 +0200

    example

---
+++ @MissionFile/config.cpp
@@ -0,0 +1,14 @@
+
+ // add this class into config.cpp of your mission file
+class CfgAdminToolkitCustomMod {
+	AdminToolkit_MenuTitle[] = {"Players", "Vehicles", "Weapons", "Exile", "Others", "Items"};
+    AdminToolkit_ModEnable = "exile";
+    AdminToolkit_Mod_Vehicles = "atk\AdminToolkit_Vehicles_Exile.sqf";
+    AdminToolkit_Mod_Custom = "atk\AdminToolkit_Exile_Exile.sqf";
+    /*
+	AdminToolkit_Mod_Players = "atk\AdminToolkit_Players_Exile.sqf";
+    AdminToolkit_Mod_Weapons = "atk\AdminToolkit_Weapons_Exile.sqf";
+    AdminToolkit_Mod_Other = "atk\AdminToolkit_Other_Exile.sqf";
+    AdminToolkit_Mod_Items = "atk\AdminToolkit_Items_Exile.sqf";*/
+};
+
