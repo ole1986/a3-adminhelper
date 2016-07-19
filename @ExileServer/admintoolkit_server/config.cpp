43282a8b7a7faa024d53cc463e6da107688f2b96
Warsheep-GER <xworshipxger@gmail.com>
2016-07-19 02:20:46 +0200

    new commands

--- @ExileServer/admintoolkit_server/config.cpp
+++ @ExileServer/admintoolkit_server/config.cpp
@@ -39,14 +39,47 @@ class CfgSettings
         /**
          * list of allowed admins using its player UID (default: me)
          */
-        AdminList[] = {"76561198037325738"};
+        AdminList[] = {"xxxxx"};
         /**
          * list of admins with restricted access only
          */
-        ModeratorList[] = {};
+        ModeratorList[] = {"xxxxx","xxxxx"};
         /**
 		 * allowed commands for moderators (default: login, teleport to a player, get a vehicle, get player list, spectate a player)
 		 */
-        ModeratorCmds[] = {"login","getplayers","tp2player", "getvehicle", "kickplayer", "specplayer", "specstop"};
+        //ModeratorCmds[] = {"login","getplayers","tp2player", "getvehicle", "kickplayer", "specplayer", "specstop"};
+		//ModeratorCmds[] = {"login", "getplayers", "tp2player", "tpplayer", "tp2pos", "getvehicle", "givevehicle", "exile_getvehicle", "getweapon", "getammo", "kickplayer", "playersCallback", "specplayer", "specstop", "exile_getmoney","exile_delvehicle", "exile_getscore"};
+        ModeratorCmds[] = {
+			"login",
+			"kickplayer",
+			"banplayer", 
+			"getplayers", 
+			"tp2pos", 
+			"tp2player", 
+			"tpplayer", 
+			"getammo", 
+			"getweapon", 
+			"getvehicle", 
+			//"givevehicle", 
+			"godmodeoff", 
+			"godmodeon", 
+			"specplayer", 
+			"specstop", 
+			//"build",
+			//"buildremove",
+			//"buildpersistent",
+			//"buildinfopersistent",
+			//"clearpersistent",
+			//"savepersistent",
+			//"buildabort"
+			"exile_getvehicle",
+			"exile_repvehicle",
+			"exile_delvehicle",
+			"exile_destvehicle",
+			"exile_getmoney",
+			"exile_getheal",
+			"exile_getscore",
+			"exile_kill"
+		};
     };
 };
